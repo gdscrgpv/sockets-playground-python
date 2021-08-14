83c087725386229134dc7af5469e67941515e4c0
apoorvpandey0 <apoorvpandey0@gmail.com>
2021-08-14 15:24:04 +0530

    Improved commenting

--- simple-sockets/client.py
+++ simple-sockets/client.py
@@ -5,9 +5,16 @@ import socket
 HOST = '127.0.0.1'  # The server's hostname or IP address
 PORT = 65432        # The port used by the server
 
+# AF_INET is the Internet address family for IPv4.
+# SOCK_STREAM is the socket type for TCP.
 with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+    # Connect to socket
     s.connect((HOST, PORT))
+
+    # Send data from client to server
     s.sendall(b'Hello, world')
+
+    # Receive data from server
     data = s.recv(1024)
 
 print('Received', repr(data))
--- simple-sockets/server.py
+++ simple-sockets/server.py
@@ -5,15 +5,28 @@ import socket
 HOST = '127.0.0.1'  # Standard loopback interface address (localhost)
 PORT = 65432        # Port to listen on (non-privileged ports are > 1023)
 
+# AF_INET is the Internet address family for IPv4.
+# SOCK_STREAM is the socket type for TCP.
 with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+    
+    # Binds the socket to the port
     s.bind((HOST, PORT))
+
+    # Listen for incoming connections
     s.listen()
     print("Binded to IP {} on port {}".format(HOST,PORT))
+
+    # Accept connections
     conn, addr = s.accept()
+    
     with conn:
         print('Connected by', addr)
         while True:
+
+            # Receives data from the client
             data = conn.recv(1024)
             if not data:
                 break
+
+            # Sends the data back to the client
             conn.sendall(b"server says "+data)
